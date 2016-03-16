5e0b227f35b7ff2bc0bbcb87d228a4b295e52d3d
Shawn Michaud <shawnmne@hotmail.com>
2016-03-15 22:01:34 -0500

    documented

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -15,6 +15,11 @@
 # This method should, in its final form, not do any output.
 
 
+
+# valid_year? method verifies the year is between 1880 and 2280 inclusive
+# year - year intger value to be evaluated
+# Returns pass_test boolean whether it falls in range or not
+#
 def valid_year?(year)
   pass_test = TRUE    
   if year < 1880 || year > 2280 
@@ -23,7 +28,10 @@ def valid_year?(year)
   return pass_test
  end
      
-
+#valid_month? method verifies the month is between 1 and 12 inclusive
+#month - month integer value to be evaluated
+#Returns pass_test boolean whether it falls in range or not
+#
 def valid_month?(month)
   pass_test = TRUE    
   if month < 1 || month > 12 
@@ -32,6 +40,11 @@ def valid_month?(month)
   return pass_test
 end
 
+#valid_day? method verifies the day falls between 1 and 30 or 1 and 31 depending on month
+#day - day integer value to be evaluated
+#month - month integer value to determine whether max is 30 or 31 
+#Return pass_test boolean whether it falls in range or not
+#
 def valid_day?(month, day)
   pass_test = TRUE  
   if month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12
@@ -47,7 +60,14 @@ def valid_day?(month, day)
   end
   return pass_test
 end  
-  
+
+#valid_day_february? method verifies the day is legitmitate for february and must check
+#   for leap year to determine whether 28 or 29 days
+#month - check to see if dealing with february
+#year - check to see if year is a leap year
+#day - verify day is in range. Between 1 and 28 or 1 and 29 depending on leap year
+#Returns pass_test boolean whether day falls in range or not
+# 
 def valid_day_february?(month, day, year)
   pass_test = TRUE    
   if month == 2 && !((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
@@ -64,10 +84,19 @@ def valid_day_february?(month, day, year)
   return pass_test
 end
 
+#valid_date? method verifies day, month, and year are all legitimate within a certain range
+#   year must be between 1880 and 2280 inclusive, month must be between 1 and 12, day must
+#   be at most 31 depending on month, leap years must be accounted for in February.
+#pass_year is the return boolean from valid_year? method
+#pass_month is the retun boolean from valid_month? method
+#pass_day is the return boolean from valid_day? method
+#pass_day_february? is the return boolean from valid_day_februray? method
+#Returns the boolean value of all the methods anded together
+#
 def valid_date?(month, day, year)
  pass_year = valid_year?(year)
  pass_month = valid_month?(month)
  pass_day = valid_day?(month, day)
  pass_day_february = valid_day_february?(month, day, year)
-  return pass_year && pass_month && pass_day && pass_day_february
+ return pass_year && pass_month && pass_day && pass_day_february
 end 
